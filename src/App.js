6a85669f29b93c65ac0915f701576411fda41dca
orimazrafi <orimazrafi@gmail.com>
2020-03-16 12:29:45 +0200

    Upload the complete app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { PhoneFormat } from "./components/PhoneFormat";
+import { UseNumberHook } from "./hooks/UseNumberHook";
 
 function App() {
+  const [number, handleChange] = UseNumberHook();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PhoneFormat onChange={handleChange} />
+        {number}
       </header>
     </div>
   );
